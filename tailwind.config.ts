565cc67bc456adbd0c7971718e599e915f80b98c
springerjanek <springerjan2@gmail.com>
2026-04-05 16:07:37 +0200

    feat: font config for tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", "sans-serif"],
+        mono: ["var(--font-geist-mono)", "monospace"],
+        eiko: ["var(--font-pp-eiko)", "serif"],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
