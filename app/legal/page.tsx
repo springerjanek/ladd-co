a7f6ab86cba2da170a7367ba7ce8aec151c49c7c
springerjanek <springerjan2@gmail.com>
2026-04-06 19:12:30 +0200

    feat: legal page

---
+++ app/legal/page.tsx
@@ -0,0 +1,187 @@
+export default function LegalPage() {
+  return (
+    <div className="flex items-center justify-center p-[72px] font-light text-neutral-800">
+      <div className="max-w-[550px]">
+        <h1 className="font-heading leading-[64.4px] text-[56px] mb-6">
+          Legal
+        </h1>
+
+        <h2 className="font-heading leading-[130%] text-[32px] mb-6">
+          Business Continuity Plan
+        </h2>
+
+        <div className="text-earth font-sans text-[15px] leading-[160%] space-y-6">
+          <p>
+            Rockefeller Capital Management has developed a Business Continuity
+            Plan to manage and mitigate the impact of events that have the
+            potential to cause significant business disruption. Since the timing
+            and impact of disasters and disruptions is unpredictable, the
+            Business Continuity Plan needs to be flexible in responding to
+            actual events as they transpire. Consequently, the Business
+            Continuity Plan is designed to enable us to recover and return to
+            business operations as quickly as possible.
+          </p>
+
+          <p>
+            We believe that we have implemented reasonable measures to quickly
+            resume business operations after a significant business disruption
+            and safeguard our employees, property, and books and records. The
+            Business Continuity Plan addresses data backup and recovery, all
+            mission critical systems, financial and operational assessments,
+            alternative communications with customers, employees, and
+            regulators, alternate physical locations for our employees, critical
+            supplier, bank, and counter-party impact, and regulatory reporting
+            requirements. In short, our business continuity plan is designed to
+            permit our firm to resume operations as quickly as possible, given
+            the scope and severity of the significant business disruption.
+          </p>
+
+          <p>
+            Our clearing firm, National Financial Services, LLC, backs up our
+            important records in a geographically separate area. While every
+            emergency situation poses unique problems based on external factors,
+            such as time of day and the severity of the disruption, we have been
+            advised by our clearing firm that its objective is to restore its
+            own operations and be able to complete existing transactions and
+            accept new transactions and payments as soon as practicable. Your
+            orders and requests for funds and securities could be delayed during
+            this period. Significant business disruptions can vary in their
+            scope, such as affecting only our firm, a single building housing
+            our firm, the business district where our firm is located, the city
+            where we are located, or the whole region. Within each of these
+            areas, the severity of the disruption can also vary from minimal to
+            severe. In a disruption to only our firm or a building housing our
+            firm, we will transfer our operations to a local site when needed
+            and expect to recover and resume business. In a disruption affecting
+            our business district, city, or region, we will transfer our
+            operations to a site outside of the affected area and recover and
+            resume business as soon as practicable. In either situation, we plan
+            to continue in business, transfer operations to our clearing firm if
+            necessary, and notify you through our website (www.rockco.com) , or
+            our main number, (212) 549-5100. If the significant business
+            disruption is so severe that it prevents us from remaining in
+            business, we will assure our customers’ prompt access to their funds
+            and securities.
+          </p>
+
+          <p>
+            For more information – If you have questions about our business
+            continuity planning, you can contact us at (212) 549-5100 or e-mail
+            us at info@rockco.com.
+          </p>
+        </div>
+        <div className="mt-6 text-earth font-sans text-[15px] leading-[160%] space-y-6">
+          <h3 className="font-heading leading-[130%] text-[32px] mb-6">
+            Legal Entity Disclosure
+          </h3>
+          <p>
+            Rockefeller Capital Management is the marketing name for Rockefeller
+            Capital Management L.P. and its affiliates. Investment advisory and
+            fiduciary activities are performed by the following affiliates of
+            Rockefeller Capital Management: Rockefeller & Co. LLC, Rockefeller
+            Trust Company, N.A. and The Rockefeller Trust Company (Delaware).
+            Rockefeller Asset Management and Rockefeller Global Family Office
+            are divisions of Rockefeller & Co. LLC. Rockefeller Financial LLC is
+            dually registered as a broker dealer and registered investment
+            adviser with the US Securities Exchange Commission, Member FINRA and
+            SIPC. You may find information on the background of Rockefeller
+            Financial LLC and its financial professionals on FINRA’s BrokerCheck
+            website: 
+            <a
+              className="underline decoration-1"
+              href="http://brokercheck.finra.org/"
+            >
+              http://brokercheck.finra.org/
+            </a>
+            . Investing involves risk, including the possible loss of principal.
+            Past performance is no guarantee of future results and no investment
+            or financial planning strategy can guarantee profit or protection.
+            Neither Rockefeller Capital Management or its financial advisors
+            provide legal, tax nor accounting advice to clients. Please consult
+            your own legal and/or tax advisors in connection with financial
+            decisions.{" "}
+            <a
+              className="underline decoration-1"
+              href="https://rcmbrand.rockco.com/RCM%20-%20Notice%20to%20Canadian%20Clients%20-%202020.pdf"
+            >
+              Notice to Canadian Clients
+            </a>
+            <br></br>
+            Routing and Execution Disclosures Made Pursuant to Rule of SEC
+            Regulation NMS Rule 606 of SEC Regulation NMS requires
+            broker-dealers receiving non-directed client orders to publicly
+            disclose, on a quarterly basis, the top execution venues to which
+            such orders are routed for execution. In addition, broker-dealers
+            must disclose material aspects of the relationships they maintain
+            with the identified execution venues.   Rockefeller Financial LLC
+            client orders in NMS securities are executed by and through its
+            fully disclosed clearing and custodial firm, National Financial LLC
+            (member NYSE\SIPC). The Rule 606 statistics for Rockefeller
+            Financial LLC for the period up to and including Q1 2024 is
+            available for public review by clicking{" "}
+            <a
+              className="underline decoration-1"
+              href="https://mta.ihsmarkit.com/app-v2/public-report-library/public-report-library-view/Rockefeller%20Capital%20Management/422"
+            >
+              here
+            </a>
+            .<br></br> The Rule 606 statistics for Rockefeller Financial LLC for
+            the period from and including Q2 2024 is available for public review
+            by clicking{" "}
+            <a
+              className="underline decoration-1"
+              href="https://www.finra.org/finra-data/606-nms-data"
+            >
+              here
+            </a>
+            <br></br>. Disclaimer. Rockefeller Financial LLC and its affiliates
+            do not represent, warrant, or guarantee that the Reports are
+            accurate. Rockefeller disclaims liability for any direct, indirect,
+            punitive, special, consequential, or incidental damages related to
+            the Reports or the use of the Reports. The information provided in
+            the Reports may be impacted by market data system outages or errors,
+            both internal and external, and it is dependent upon the integrity
+            and accuracy of data provided by outside sources. Certain
+            assumptions have been made in preparing the Reports, and changes to
+            the assumptions may have a material impact on results. The Reports
+            do not endorse or recommend any particular security or market
+            participant. This disclaimer applies to the Reports in their
+            entirety, irrespective of whether the Reports are used or viewed in
+            whole or in part, and may be reviewed at any time by accessing the
+            “Disclaimer” posted on the web page where the Reports are made
+            available.<br></br>COPYRIGHT ©Rockefeller Capital Management. All
+            rights reserved.
+          </p>
+          <p>
+            Rockefeller Capital Management is the marketing name for Rockefeller
+            Capital Management L.P. and its affiliates. Investment advisory and
+            fiduciary activities are performed by the following affiliates of
+            Rockefeller Capital Management: Rockefeller & Co. LLC, Rockefeller
+            Trust Company, N.A. and The Rockefeller Trust Company (Delaware).
+            Rockefeller Asset Management and Rockefeller Global Family Office
+            are divisions of Rockefeller & Co. LLC. Rockefeller Financial LLC is
+            dually registered as a broker dealer and registered investment
+            adviser with the US Securities Exchange Commission, Member FINRA and
+            SIPC. You may find information on the background of Rockefeller
+            Financial LLC and its financial professionals on FINRA’s BrokerCheck
+            website: http://brokercheck.finra.org/. Investing involves risk,
+            including the possible loss of principal. Past performance is no
+            guarantee of future results and no investment or financial planning
+            strategy can guarantee profit or protection. Neither Rockefeller
+            Capital Management or its financial advisors provide legal, tax nor
+            accounting advice to clients. Please consult your own legal and/or
+            tax advisors in connection with financial decisions.
+          </p>
+          <p>
+            This disclaimer applies to the Reports in their entirety,
+            irrespective of whether the Reports are used or viewed in whole or
+            in part, and may be reviewed at any time by accessing the
+            “Disclaimer” posted on the web page where the Reports are made
+            available.
+          </p>
+          <p>COPYRIGHT ©Rockefeller Capital Management. All rights reserved.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
